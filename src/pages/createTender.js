0d1ccd5410a4aa93e7ae3f3f7dd4437f1e71cef5
Rumira Daksith <rumirad@null.net>
2022-08-19 00:25:47 +0530

    Create tender integrated

--- src/pages/createTender.js
+++ src/pages/createTender.js
@@ -1,22 +1,93 @@
 import Footer from "../component/footer";
 import navbarImage from "../assets/full_logo.png";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Navbar2 from "../component/navbar2";
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 function CreateTender() {
+    const [tenderTitle, setTenderTitle] = useState("");
+    const [tenderType, setTenderType] = useState("Select Your Interest");
+    const [publishDate, setPublishDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+    const [province, setProvince] = useState("Select Your Province");
+
+    const [image, setImage] = useState("");
+    const [imageUrl, setImageUrl] = useState(null);
+    const [imagePreviewUrl, setImagePreviewUrl] = useState('');
+
+    const [attachment, setAttachment] = useState("");
+    const [attachmentUrl, setAttachmentUrl] = useState(null);
+    const [attachmentPreviewUrl, setAttachmentPreviewUrl] = useState('');
+
+    const [description, setDescription] = useState("");
+
+    useEffect(() => {
+        console.log("endDate", endDate);
+    }, [endDate]);
+
+    const getHTMLdate = () => {
+        const today = new Date();
+        var d = new Date(today),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2)
+            month = '0' + month;
+        if (day.length < 2)
+            day = '0' + day;
+
+        return [year, month, day].join('-');
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const res = await axios.post(`${process.env.REACT_APP_API_DOMAIN}/tender`, {
+            title: tenderTitle,
+            description: description,
+            category: tenderType,
+            location: province,
+            expirydate: endDate,
+            image: imageUrl,
+            document: attachmentUrl,
+        }, {
+            headers: {
+                'x-auth-token': localStorage.getItem("token"),
+            }
+        });
+        if (res.data.error) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: res.data.error,
+            })
+        } else {
+            Swal.fire({
+                title: "Success",
+                text: "Tender Created Successfully",
+                icon: "success",
+                confirmButtonText: "OK",
+            });
+        }
+
+
+
+    }
     return (
         <>
 
             {/*Start Header*/}
-           <Navbar2/>
+            <Navbar2 />
             {/*End Header*/}
 
             {/*Start Section 1*/}
             <section className="section" id="post-tender-background">
                 <div className="columns is-justify-content-center">
                     <div className="column is-two-thirds ">
-                        <form className="box" id="tender-post-form">
+                        <form className="box" id="tender-post-form" onSubmit={handleSubmit}>
                             <h2 className="subtitle is-size-4 has-text-centered has-text-weight-bold">
                                 Create Your Tender
                             </h2>
@@ -24,7 +95,9 @@ function CreateTender() {
                                 <label className="label">Tender Title</label>
                                 <div className="control">
                                     <input className="input is-primary" type="text"
-                                           placeholder="e.g. Supply of Medical Equipment"/>
+                                        value={tenderTitle}
+                                        onChange={(e) => setTenderTitle(e.target.value)}
+                                        placeholder="e.g. Supply of Medical Equipment" />
                                 </div>
                             </div>
 
@@ -33,12 +106,14 @@ function CreateTender() {
                                     <div className="field">
                                         <label className="label">Your Tender Type</label>
                                         <div className="select is-primary">
-                                            <select>
+                                            <select
+                                                onChange={(e) => setTenderType(e.target.value)}
+                                                value={tenderType}
+                                            >
                                                 <option disabled selected>Select Your Interest</option>
                                                 <option value="Education">Education</option>
                                                 <option value="Packaging">Packaging</option>
-                                                <option value="Supplier Registration">Supplier Registration
-                                                </option>
+                                                <option value="Supplier Registration">Supplier Registration</option>
                                                 <option value="Services">Services</option>
                                                 <option value="Medical">Medical</option>
                                                 <option value="Expression of Interests(EOI)">Expression of
@@ -92,7 +167,11 @@ function CreateTender() {
                                 <div className="column">
                                     <label className="label">Province</label>
                                     <div className="select is-primary">
-                                        <select>
+                                        <select
+                                            onChange={(e) => setProvince(e.target.value)}
+                                            defaultValue="Select Your Province"
+                                            value={province}
+                                        >
                                             <option disabled selected>Select Your Province</option>
                                             <option>Central Province</option>
                                             <option>North Central Province</option>
@@ -111,51 +190,126 @@ function CreateTender() {
                             <div className="columns">
                                 <div className="column">
                                     <label className="label">Publishing Date</label>
-                                    <input className="input is-primary" type="date"/>
+                                    <input
+                                        className="input is-primary"
+                                        type="date"
+                                        value={getHTMLdate()}
+                                        onChange={(e) => setPublishDate(e.target.value)}
+                                        disabled={true}
+                                    />
                                 </div>
                                 <div className="column">
                                     <label className="label">Closing Date</label>
-                                    <input className="input is-primary" type="date"/>
+                                    <input
+                                        className="input is-primary"
+                                        type="date"
+                                        value={endDate}
+                                        onChange={(e) => setEndDate(e.target.value)}
+                                        min={getHTMLdate()}
+                                    />
+
                                 </div>
                             </div>
 
                             <div className="file has-name is-info">
                                 <label className="file-label">
-                                    <input className="file-input" type="file" name="resume"/>
+                                    <input
+                                        className="file-input"
+                                        type="file"
+                                        name="resume"
+                                        accept="image/*"
+                                        onChange={(e) => {
+                                            //
+                                            const reader = new FileReader();
+                                            reader.onloadend = () => {
+                                                setImagePreviewUrl(reader.result);
+                                            }
+                                            reader.readAsDataURL(e.target.files[0]);
+                                            console.log(e.target.files[0]);
+                                            const formData = new FormData();
+                                            formData.append('file', e.target.files[0]);
+                                            axios.post(`${process.env.REACT_APP_API_DOMAIN}/file-upload`, formData, {
+                                                headers: {
+                                                    'Content-Type': 'multipart/form-data',
+                                                    'x-auth-token': localStorage.getItem('token')
+                                                }
+                                            }).then((res) => {
+                                                console.log(res.data);
+                                                // set filename to file
+                                                setImage(e.target.files[0].name);
+                                                setImageUrl(res.data.data);
+                                            });
+                                        }}
+                                    />
                                     <span className="file-cta">
-                                          <span className="file-icon">
+                                        <span className="file-icon">
                                             <i className="fas fa-upload"></i>
-                                          </span>
-                                          <span className="file-label">
+                                        </span>
+                                        <span className="file-label">
                                             Upload Your Picture…
-                                          </span>
                                         </span>
-                                    <span className="file-name">
-                                        </span>
+                                    </span>
+                                    <span className="file-name" style={{ width: '100%' }}>
+                                        {image ? image + ' has been uploaded' : 'No file uploaded'}
+                                    </span>
+                                    {/* {imagePreviewUrl ? <img src={imagePreviewUrl} alt="preview" style={{ width: '100%' }} /> : ""} */}
                                 </label>
                             </div>
-                            <br/>
+                            <br />
 
                             <div className="file has-name is-boxed is-warning">
                                 <label className="file-label">
-                                    <input className="file-input" type="file" name="resume"/>
+                                    <input
+                                        className="file-input"
+                                        type="file"
+                                        name="resume"
+                                        // accept only pdf and docx files or exel files
+                                        accept="application/pdf,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel"
+                                        onChange={(e) => {
+                                            const reader = new FileReader();
+                                            reader.onloadend = () => {
+                                                setAttachmentPreviewUrl(reader.result);
+                                            }
+                                            reader.readAsDataURL(e.target.files[0]);
+                                            console.log(e.target.files[0]);
+                                            const formData = new FormData();
+                                            formData.append('file', e.target.files[0]);
+                                            axios.post(`${process.env.REACT_APP_API_DOMAIN}/file-upload`, formData, {
+                                                headers: {
+                                                    'Content-Type': 'multipart/form-data',
+                                                    'x-auth-token': localStorage.getItem('token')
+                                                }
+                                            }).then((res) => {
+                                                console.log(res.data);
+                                                // set filename to file
+                                                setAttachment(e.target.files[0].name);
+                                                setAttachmentUrl(res.data.data);
+                                            });
+                                        }}
+                                    />
                                     <span className="file-cta">
-                                          <span className="file-icon">
+                                        <span className="file-icon">
                                             <i className="fas fa-upload"></i>
-                                          </span>
-                                          <span className="file-label">
+                                        </span>
+                                        <span className="file-label">
                                             Upload Tender file…
-                                          </span>
                                         </span>
+                                    </span>
                                     <span className="file-name">
-
-                                        </span>
+                                        {/* {imagePreviewUrl ? <img src={imagePreviewUrl} alt="preview" style={{ width: '100%' }} /> : ""} */}
+                                        {attachment ? attachment + ' has been uploaded' : 'No file uploaded'}
+                                    </span>
                                 </label>
                             </div>
-                            <br/>
+                            <br />
                             <label className="label">Description About Tender</label>
-                            <textarea className="textarea is-primary" placeholder="Our tender is.."></textarea>
-
+                            <textarea
+                                className="textarea is-primary"
+                                placeholder="Our tender is.."
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                            >
+                            </textarea>
                             <div className="mt-4 column has-text-centered">
                                 <button className="button is-primary is-size-6	has-text-weight-bold">Create Your Tender</button>
                             </div>
